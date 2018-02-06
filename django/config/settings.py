797c4b10c5774ce56500d91faf4eebae40829911
park chan <parkchan428@gmail.com>
2018-02-06 16:31:16 +0900

    app artist, song, album 추가

--- django/config/settings.py
+++ django/config/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'artist',
+    'song',
+    'album',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
