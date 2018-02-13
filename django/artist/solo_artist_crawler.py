88dbf2ccf514c5992c9cff4db2c05f5084154e41
park chan <parkchan428@gmail.com>
2018-02-13 13:42:52 +0900

    name 길이변수 추가

--- django/artist/solo_artist_crawler.py
+++ django/artist/solo_artist_crawler.py
@@ -26,18 +26,26 @@ class artist_crawler:
         dl_list = soup.find("div", {"class": "section_atistinfo04"}).findAll("dd")
 
         img_profile = div_list.find("span", {"id": "artistImgArea"}).img['src']
-        name = div_list.find("div", {"class": "wrap_atist_info"}).p.text[5:]
-        """ name 본명 잘라야됌"""
+        name = div_list.find("div", {"class": "wrap_atist_info"}).p.text[5:].split()
+        
+        if len(name) == 1:
+            name = name[0]
+        elif len(name) == 2:
+            name = name[0]
+        elif len(name) == 3:
+            name = name[0]+name[1]
+        else:
+            name = name[0]+name[1]+name[2]
+
         real_name = dl_list[0].text
         nationality = dl_list[1].text
         birth_date = dl_list[2].text
         constellation = dl_list[4].text
         blood_type = dl_list[5].text
         intro = soup.find("div", {"id": "d_artist_intro"}).text.strip()
-
         result = {
-            "img_profile": img_profile,
             "name": name,
+            "img_profile": img_profile,
             "real_name": real_name,
             "nationality": nationality,
             "birth_date": birth_date,
@@ -49,6 +57,7 @@ class artist_crawler:
 
 
 if __name__ == "__main__":
-    artist_id = 905701
+    artist_id = 1191
+    # artist_id = 160816
     ac = artist_crawler
     ac.melon_artist_crawler(artist_id)
