81f4fecbe8e65b4302f605532e541ebed2d5aef7
park chan <parkchan428@gmail.com>
2018-02-16 17:28:39 +0900

    dl_list갯수에 따라 for문으로 데이터 출력

--- django/artist/solo_artist_crawler.py
+++ django/artist/solo_artist_crawler.py
@@ -21,7 +21,7 @@ class artist_crawler:
         url = f"https://www.melon.com/search/total/index.htm?q={name}&section=&ipath=srch_form"
         response = requests.get(url)
         soup = BeautifulSoup(response.text, "lxml")
-        artist_id = soup.find("div", {"class":"info_01"}).input['value']
+        artist_id = soup.find("div", {"class": "info_01"}).input['value']
 
         '''
         위쪽은 이름으로 검색하는법
@@ -35,7 +35,8 @@ class artist_crawler:
         source = response.text
         soup = BeautifulSoup(source, "lxml")
         div_list = soup.find("div", {"id": "conts"})
-        dl_list = soup.find("div", {"class": "section_atistinfo04"}).findAll("dd")
+        dl_list = soup.find("div", {"class": "section_atistinfo04"}).find("dl")
+        dd_list = soup.find("div", {"class": "section_atistinfo04"}).findAll("dd")
         dt_list = soup.find("div", {"class": "section_atistinfo04"}).findAll("dt")
         img_profile = div_list.find("span", {"id": "artistImgArea"}).img['src']
         name = div_list.find("div", {"class": "wrap_atist_info"}).p.text[5:].split()
@@ -48,31 +49,68 @@ class artist_crawler:
             name = name[0] + name[1]
         else:
             name = name[0] + name[1] + name[2]
-        real_name = dl_list[0].text
+
+        real_name = dd_list[0].text
         if dt_list[0].text == "본명":
             real_name = real_name
         else:
             real_name = ""
 
-        nationality = dl_list[1].text
-        birth_date = dl_list[2].text
-        constellation = dl_list[4].text
-        blood_type = dl_list[5].text
+
         intro = soup.find("div", {"id": "d_artist_intro"}).text.strip()
-        result = {
-            "name": name,
-            "img_profile": img_profile,
-            "real_name": real_name,
-            "nationality": nationality,
-            "birth_date": birth_date,
-            "constellation": constellation,
-            "blood_type": blood_type,
-            "intro": intro,
-        }
+
+        k = len(dd_list)
+        result = []
+        result.append({'name': name})
+        for i in range(k):
+            result.append({
+                dt_list[i].text: dd_list[i].text
+            })
+        result.append({'intro': intro})
+        # print(result)
         return result
+
+'''
+데이터가 리스트의 dict처럼 나옴
+'''
+        # n = 0
+        # result = []
+        # while True:
+        #     result.append({
+        #         dt_list[n].text: dd_list[n].text
+        #     })
+        #     n += 1
+        #     if len(dd_list) == n:
+        #         break
+        # print(result)
+
+
+'''
+dd_list6개 아닐경우 에러남
+'''
+        # nationality = dd_list[1].text
+        # birth_date = dd_list[2].text
+        # constellation = dd_list[4].text
+        # blood_type = dd_list[5].text
+        # intro = soup.find("div", {"id": "d_artist_intro"}).text.strip()
+        # result = {
+        #     "name": name,
+        #     "img_profile": img_profile,
+        #     "real_name": real_name,
+        #     "nationality": nationality,
+        #     "birth_date": birth_date,
+        #     "constellation": constellation,
+        #     "blood_type": blood_type,
+        #     "intro": intro,
+        #     }
+        # return result
+
+
 
 
 if __name__ == "__main__":
     name = input("이름을 입력하세요")
+    # name = "박효신"
     ac = artist_crawler.melon_artist_crawler(name)
-    print(ac)
+    for i in ac:
+        print(i)
