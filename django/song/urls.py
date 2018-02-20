66956dd8caecc9113d5a460bdb1f9a5c30a1dc41
park chan <parkchan428@gmail.com>
2018-02-20 11:31:08 +0900

    son_search template에 url태그 추가

--- django/song/urls.py
+++ django/song/urls.py
@@ -6,5 +6,5 @@ from . import views
 app_name = 'song'
 urlpatterns = [
     path('', views.song_list, name='song-list'),
-    path('search/', views.song_search, name='song-search')
+    path('search/', views.song_search, name='song-search'),
 ]
