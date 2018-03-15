ab8294807430be0f0f255445a8cd37472bd900d9
park chan <parkchan428@gmail.com>
2018-03-15 15:30:08 +0900

    artist api완성

--- app/artist/apis/artist/list.py
+++ app/artist/apis/artist/list.py
@@ -1,14 +1,40 @@
-from django.http import JsonResponse
+import json
+
+from django.http import JsonResponse, HttpResponse
 
 from artist.models import Artist
 
+__all__ = (
+    'artist_list',
+)
+
 
 def artist_list(request):
-    # localhost:8000/api/artist/
-    artists = Artist.objects.all()
-    data = {
-        'artists': artists,
+    """
+    data: {
+        'artist': [
+            {
+                'melon_id':.. ,
+                'name': ..
+            },
+            {
+                'melon_id':.. ,
+                'name': ..
+            }
+            ...
+        ]
     }
+    :param request:
+    :return:
+    """
+    artists = Artist.objects.all()
+    data = {'artist': [{'melon_id': artist.melon_id, 'name': artist.name} for artist in artists]}
+
+    # for artist in artists:
+    #     data.append({'melon_id': artist.melon_id,
+    #                  'name': artist.name})
+
+    # return HttpResponse(json.dumps(data), content_type='application/json')
     return JsonResponse(data)
 
 # /artist/       -> artist.urls.views
